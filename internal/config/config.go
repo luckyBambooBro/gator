b789636bd7bebb0c3a194487883a3c764000aa1c
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-03-20 14:45:08 +1030

    corrected read(), still gotta do setuser()

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 const configFileNameBase string = ".gatorconfig.json"
@@ -11,6 +12,36 @@ type Config struct {
 	DBURL string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
+
+func Read() (Config, error) {
+	//get file path of .gatorconfig.json
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	//open .gatorconfig.json
+	file, err := os.Open(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+	//decode .gatorconfig.json into Config struct and return
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	if err = decoder.Decode(&cfg); err != nil {
+		return Config{}, err
+	}
+	return cfg, nil
+}
+
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, configFileNameBase), nil
+}
+
 
 func (cfg *Config) SetUser(username string) error {
 	cfg.CurrentUserName = username
@@ -23,6 +54,7 @@ func (cfg *Config) SetUser(username string) error {
 	if err != nil {
 		return err
 	}
+	os.Create(filepath)
 	//write to file (overwrite .gatorconfig.json with new data)
 	os.WriteFile(filepath, data, 0644)
 	return nil
@@ -30,6 +62,7 @@ func (cfg *Config) SetUser(username string) error {
 }
 
 func write(cfg *Config) ([]byte, error){
+	
 	data, err := json.Marshal(cfg)
 	if err != nil {
 		return nil, err
