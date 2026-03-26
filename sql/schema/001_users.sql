a5875add8ed5c773550d67358d74ee1df80ed765
luckyBambooBro <huypham008@hotmail.com>
2026-03-26 16:58:30 +1030

    up to same as previous commit

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at timestamp (2) NOT NULL,
+    updated_at timestamp (2) NOT NULL,
+    name TEXT NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
