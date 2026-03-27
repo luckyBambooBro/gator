65cd5a06d37a110b6ced67d7c0c937d832388253
luckyBambooBro <huypham008@hotmail.com>
2026-03-28 07:34:41 +1030

    ch2L3 up to q6

--- handler_user.go
+++ handler_user.go
@@ -6,7 +6,7 @@ import (
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) != 1 { //or should i have if len(cmd.Args) != 1 ... (since we want just one argument for username?)
-		return fmt.Errorf("usage: <%s> username not provided", cmd.Name)
+		return fmt.Errorf("usage: <%s> username error. Please provide one username", cmd.Name)
 	}
 	if err := s.cfg.SetUser(cmd.Args[0]); err != nil {
 		return err
