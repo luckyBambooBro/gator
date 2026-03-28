2a524a8826e77aaab878c06ccf114a4b97ba7f45
luckyBambooBro <huypham008@hotmail.com>
2026-03-29 08:10:35 +1030

    attempting q8 in handler_user.go

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+
+	"github.com/luckyBambooBro/gator/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -13,4 +16,22 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	fmt.Println("username switched successfully")
 	return nil
+}
+
+/*
+Ensure that a name was passed in the args.
+Create a new user in the database. It should have access to the CreateUser query through the state -> db struct.
+Pass context.Background() to the query to create an empty Context argument.
+Use the uuid.New() function to generate a new UUID for the user.
+created_at and updated_at should be the current time.
+Use the provided name.
+Exit with code 1 if a user with that name already exists.
+Set the current user in the config to the given name.
+Print a message that the user was created, and log the user's data to the console for your own debugging.
+*/
+func handlerRegister(s *state,c command) error {
+	name := c.Args[0]
+	if name == "" {
+		return errors.New("name not provided for register command")
+	}
 }
