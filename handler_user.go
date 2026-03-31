b576c37694f7b00076aeb7a4c085fff96bd1dcea
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-03-31 17:19:07 +1030

    checked solutions ready to move onto Ch2L4

--- handler_user.go
+++ handler_user.go
@@ -31,18 +31,6 @@ func handlerLogin(s *state, cmd command) error {
 	return nil
 }
 
-/*
-Ensure that a name was passed in the args.
-
-Create a new user in the database. It should have access to the CreateUser query through the state -> db struct.
-Pass context.Background() to the query to create an empty Context argument.
-Use the uuid.New() function to generate a new UUID for the user.
-created_at and updated_at should be the current time.
-Use the provided name.
-Exit with code 1 if a user with that name already exists.
-Set the current user in the config to the given name.
-Print a message that the user was created, and log the user's data to the console for your own debugging.
-*/
 func handlerRegister(s *state, c command) error {
 	if len(c.Args) == 0 {
 				return errors.New("name not provided for register command")
