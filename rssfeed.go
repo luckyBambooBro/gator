15abd587b444f18cd706f684b1f6e36e8c3c6e49
luckyBambooBro <huypham008@hotmail.com>
2026-04-03 15:10:22 +1030

    started Ch3L1

---
+++ rssfeed.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"net/http"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(context.Background(), "GET", feedURL, nil)
+}
