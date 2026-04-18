bab29310377b10ffce876cb7005430553740120d
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-04-19 06:33:12 +0930

    commit error. this one is good

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -19,3 +19,13 @@ ON feeds.user_id = users.id;
 -- name: GetFeedByURL :one
 SELECT * FROM feeds
 WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET updated_at = $1, last_fetched_at = $1
+WHERE id = $2;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
